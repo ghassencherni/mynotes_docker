fa4ef2c09b46231878b6553ddb724056c2a82aca
ghassen charni <ghassen.cherni@gmail.com>
2020-05-10 15:57:04 +0200

    modify docker entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
 flask db init
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  echo success
-else
-  echo "DB already configured"; exit 0
-fi
+exec "$@"
 
 sleep 5
 # Migrate the database
 flask db migrate
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-  echo success
-else
-  echo "DB already configured"; exit 0
-fi
+exec "$@"
+
 sleep 5
 
 # Upgrade the dababase
 flask db upgrade 
-exit 0
-
 exec "$@"
