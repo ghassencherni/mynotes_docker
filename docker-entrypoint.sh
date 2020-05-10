38edddc2372bb7ce290d343c4853af03163ac75f
ghassen charni <ghassen.cherni@gmail.com>
2020-05-10 14:17:13 +0200

    modify replica

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "DB HOST is $DB_HOST"
-flask db init
+echo "DB HOST is $DB_HOST";
+flask db init || true
 sleep 5
 
 # Migrate the database
-flask db migrate
+flask db migrate || true
 sleep 5
 
 # Upgrade the dababase
-flask db upgrade
+flask db upgrade || true
 
